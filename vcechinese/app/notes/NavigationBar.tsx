88dae9a943cc8ee0a8d90c76947023982afd44c6
Ryan Chan <ryachan@student.unimelb.edu.au>
2024-03-25 20:53:09 +1100

    Changed navbar to white background

--- vcechinese/app/notes/NavigationBar.tsx
+++ vcechinese/app/notes/NavigationBar.tsx
@@ -4,22 +4,18 @@ import { MoonFillIcon } from "../_assets/Icons";
 
 const NavigationBar = () => {
   return (
-    <div className="w-screen flex flex-row bg-[#202123] text-white text-[11px]">
-      <EmptySpace />
-      <Text text="Feedback" />
+    <div className="w-screen flex flex-row text-[11px] py-0.5 translate-y-2">
       <div className="grow"></div>
       <div className="flex flex-row items-center">
-        <div className="h-2.5 px-2.5 fill-[#9437FF]">
+        <div className="h-2.5 px-2.5 fill-black hover:fill-[#9437FF]">
           <MoonFillIcon />
         </div>
-        <div className="flex flex-row">
-          <Text text="Notes" />
+        <div className="flex flex-row pr-8">
           <Text text="Forums" />
           <Text text="AskAI" />
           <Text text="Resources" />
           <Text text="Tutoring" />
         </div>
-        <EmptySpace />
       </div>
     </div>
   );
@@ -29,16 +25,10 @@ function Text(props: { text: string }) {
   return (
     <div>
       <Link href={`/${props.text.toLowerCase()}`}>
-        <div className="px-2.5 pt-[3.5px] pb-[2px] hover:underline">
-          {props.text}
-        </div>
+        <div className="px-2.5 py-[2px] hover:underline">{props.text}</div>
       </Link>
     </div>
   );
-}
-
-function EmptySpace() {
-  return <div className="px-2"></div>;
 }
 
 export default NavigationBar;
