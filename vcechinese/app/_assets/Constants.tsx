22243fe7e148581e4ff3280cfb1a8375f1c57193
Ryan Chan <ryachan@student.unimelb.edu.au>
2024-04-19 13:57:10 +1000

    Making sidebar

--- vcechinese/app/_assets/Constants.tsx
+++ vcechinese/app/_assets/Constants.tsx
@@ -6,13 +6,17 @@ export enum PAGE {
   TUTORING = "Tutoring",
 }
 
+export enum COLORS {
+  BRIGHT_BLUE = "#0066FF",
+}
+
 export const HEADER_LOGO_WIDTH = "285px";
 export const HEADER_BAR_HEIGHT = "75px";
 export const HEADER_BAR_WIDTHS = new Map([
   [PAGE.NOTES, "w-[1050px]"],
-  [PAGE.FORUMS, "w-full pl-[60px] pr-[42px]"],
+  [PAGE.FORUMS, "w-full pl-[80px] pr-[42px]"],
 ]);
 
 export const NOTES_CONTENTS_WIDTH = "285px";
 
-export const FORUMS_SIDEBAR_WIDTH = "170px";
+export const FORUMS_SIDEBAR_WIDTH = "158px";
--- vcechinese/app/forums/Sidebar.tsx
+++ vcechinese/app/forums/Sidebar.tsx
@@ -1,23 +1,6 @@
 import React from "react";
-import {
-  DotsConnectionFillIcon,
-  DotsConnectionIcon,
-  FileFillIcon,
-  FileIcon,
-  InfoFillIcon,
-  InfoIcon,
-  LayersStackFillIcon,
-  LayersStackIcon,
-  MegaphoneFillIcon,
-  MegaphoneIcon,
-  PenFillIcon,
-  PenIcon,
-  SoundwaveIcon,
-  TrophyFillIcon,
-  TrophyIcon,
-  VerticalLine,
-} from "../_assets/Icons";
-import { FORUMS_SIDEBAR_WIDTH } from "../_assets/Constants";
+import * as Icons from "../_assets/Icons";
+import { COLORS, FORUMS_SIDEBAR_WIDTH } from "../_assets/Constants";
 
 export default function Sidebar() {
   return (
@@ -25,32 +8,92 @@ export default function Sidebar() {
       className="flex flex-row h-screen"
       style={{ width: FORUMS_SIDEBAR_WIDTH }}
     >
-      <div>
-        {Object.values(FORUM_TOPIC).map((topic, index) => {
-          const config = getTopicConfig(topic, "w-[12px] h-[12px]");
-          return config == null ? null : (
-            <Topic
-              key={index}
-              topic={topic}
-              color={config.color}
-              icon={config.fillIcon}
-            />
-          );
-        })}
+      <div className="pl-[11px] py-[30px] space-y-8">
+        <NewThreadButton />
+        <QuickLinks />
+        <TopicsList />
       </div>
       <div className="grow"></div>
-      <VerticalLine />
+      <Icons.VerticalLine />
     </div>
   );
 }
 
-function Topic(props: { topic: string; color: string; icon: JSX.Element }) {
+function NewThreadButton() {
   return (
-    <div className="flex flex-row items-center text-gray-700 fill-gray-700 opacity-80 gap-2 py-[4px]">
-      {props.icon}
-      <button className="text-[11px] hover:underline">{props.topic}</button>
+    <div className="flex flex-row w-[70px] items-center justify-center gap-1.5 rounded-md">
+      <Icons.PlusIcon className={size} />
+      <p className="text-xs">New</p>
     </div>
   );
+}
+
+function QuickLinks() {
+  const links = [
+    new ForumTopic(
+      "All threads",
+      "#111111",
+      <Icons.ListIcon className={size} />,
+      <Icons.ListIcon className={size} />
+    ),
+    new ForumTopic(
+      "My activity",
+      "#111111",
+      <Icons.CalendarIcon className={size} />,
+      <Icons.CalendarIcon className={size} />
+    ),
+  ];
+
+  return (
+    <div>
+      {links.map((link, index) => (
+        <Topic key={index} topic={link} />
+      ))}
+    </div>
+  );
+}
+
+function TopicsList() {
+  return (
+    <div>
+      {Object.values(FORUM_TOPIC).map((topic, index) => {
+        const forumTopic = getTopicConfig(topic, size);
+        return forumTopic == null ? null : (
+          <Topic key={index} topic={forumTopic} />
+        );
+      })}
+    </div>
+  );
+}
+
+function Topic(props: { topic: ForumTopic }) {
+  return (
+    <div className="flex flex-row items-center text-gray-700 fill-gray-700 opacity-80 gap-2 py-[3.6px]">
+      {props.topic.fillIcon}
+      <button className="text-[11.8px] tracking-[-0.15px] hover:underline">
+        {props.topic.topic}
+      </button>
+    </div>
+  );
+}
+
+export class ForumTopic {
+  topic: string;
+  color: string;
+  fillIcon: JSX.Element;
+  outlineIcon: JSX.Element;
+
+  constructor(
+    topic: string,
+    color: string,
+    fillIcon: JSX.Element,
+    outlineIcon: JSX.Element
+  ) {
+    this.topic = topic;
+    this.color = color;
+    this.fillIcon = fillIcon;
+    this.outlineIcon = outlineIcon;
+  }
 }
 
 export enum FORUM_TOPIC {
@@ -64,69 +107,67 @@ export enum FORUM_TOPIC {
   SOCIAL = "Social",
 }
 
-export class ForumTopic {
-  color: string;
-  fillIcon: JSX.Element;
-  outlineIcon: JSX.Element;
-
-  constructor(color: string, fillIcon: JSX.Element, outlineIcon: JSX.Element) {
-    this.color = color;
-    this.fillIcon = fillIcon;
-    this.outlineIcon = outlineIcon;
-  }
-}
-
 export function getTopicConfig(topic: FORUM_TOPIC, className: string) {
   switch (topic) {
     case FORUM_TOPIC.ANNOUNCEMENTS:
       return new ForumTopic(
+        topic.toString(),
         "#EF4146",
-        <MegaphoneFillIcon className={className} />,
-        <MegaphoneIcon className={className} />
+        <Icons.MegaphoneFillIcon className={className} />,
+        <Icons.MegaphoneIcon className={className} />
       );
     case FORUM_TOPIC.GENERAL:
       return new ForumTopic(
+        topic.toString(),
         "#F4AB36",
-        <InfoFillIcon className={className} />,
-        <InfoIcon className={className} />
+        <Icons.InfoFillIcon className={className} />,
+        <Icons.InfoIcon className={className} />
       );
     case FORUM_TOPIC.ESSAYS:
       return new ForumTopic(
+        topic.toString(),
         "#18C27D",
-        <PenFillIcon className={className} />,
-        <PenIcon className={className} />
+        <Icons.PenFillIcon className={className} />,
+        <Icons.PenIcon className={className} />
       );
     case FORUM_TOPIC.ORAL:
       return new ForumTopic(
+        topic.toString(),
         "#5436DA",
-        <SoundwaveIcon className={className} />,
-        <SoundwaveIcon className={className} />
+        <Icons.SoundwaveIcon className={className} />,
+        <Icons.SoundwaveIcon className={className} />
       );
     case FORUM_TOPIC.SACS:
       return new ForumTopic(
+        topic.toString(),
         "#19C2FF",
-        <FileFillIcon className={className} />,
-        <FileIcon className={className} />
+        <Icons.FileFillIcon className={className} />,
+        <Icons.FileIcon className={className} />
       );
     case FORUM_TOPIC.EXAMS:
       return new ForumTopic(
+        topic.toString(),
         "#F4AB36",
-        <LayersStackFillIcon className={className} />,
-        <LayersStackIcon className={className} />
+        <Icons.LayersStackFillIcon className={className} />,
+        <Icons.LayersStackIcon className={className} />
       );
     case FORUM_TOPIC.GRADES:
       return new ForumTopic(
+        topic.toString(),
         "#EF4146",
-        <TrophyFillIcon className={className} />,
-        <TrophyIcon className={className} />
+        <Icons.GraduateFillIcon className={className} />,
+        <Icons.GraduateIcon className={className} />
       );
     case FORUM_TOPIC.SOCIAL:
       return new ForumTopic(
+        topic.toString(),
         "#18C27D",
-        <DotsConnectionFillIcon className={className} />,
-        <DotsConnectionIcon className={className} />
+        <Icons.DotsConnectionFillIcon className={className} />,
+        <Icons.DotsConnectionIcon className={className} />
       );
     default:
       return null;
   }
 }
+
+const size = "w-[13px] h-[13px]";
