24921e2672ab2a23cff838c9ce308c55ff6eaceb
Ryan Chan <ryachan@student.unimelb.edu.au>
2024-05-06 11:59:15 +1000

    Added cross origin policy

--- vcechinese/tailwind.config.js
+++ vcechinese/tailwind.config.js
@@ -2,4 +2,17 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   plugins: [require("tailwind-scrollbar"), require("numeral")],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin allow-popups",
+          },
+        ],
+      },
+    ];
+  },
 };
