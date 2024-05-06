99287b3ba204671606fb14b4039e1393a50189d4
Ryan Chan <ryachan@student.unimelb.edu.au>
2024-05-06 12:14:38 +1000

    Don't reload after signing in

--- vcechinese/app/forums/HeaderRhs.tsx
+++ vcechinese/app/forums/HeaderRhs.tsx
@@ -30,23 +30,17 @@ export function Rhs() {
       </div>
       <div className="grow"></div>
       <div className="flex gap-4">
-        {/* <div className="flex w-[300px] h-8 border-[1px] border-gray-200 rounded-full px-4 items-center translate-y-[0.5px]">
-          <SearchIcon className="w-3 h-3 fill-gray-400 mr-2" />
-          <input
-            className="w-full text-[12px] font-light outline-none"
-            type="text"
-            placeholder="Search"
-          />
-        </div> */}
-
-        {user == null ? (
+        <Logout />
+        <Login />
+        <ProfilePictureBig url={user ? user.photoURL : ""} />
+        {/* {user == null ? (
           <Login />
         ) : (
           <div className="flex flex-row items-center gap-5">
             <Logout />
             <ProfilePictureBig url={user.photoURL} />
           </div>
-        )}
+        )} */}
       </div>
     </div>
   );
