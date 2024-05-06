253c1a17921a9229fd236bacc5c9c45563a7686d
Ryan Chan <ryachan@student.unimelb.edu.au>
2024-05-06 13:06:04 +1000

    Fixed compilation error

--- vcechinese/app/auth/page.tsx
+++ vcechinese/app/auth/page.tsx
@@ -1,26 +1,11 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { UserAuthContextProvider } from "../forums/authentication/UserAuthContext";
-import { auth } from "@/firebase";
-import { User, onAuthStateChanged } from "firebase/auth";
-import Login, { Logout } from "../forums/authentication/Login";
+import AuthTesting from "./AuthTesting";
 
 export default function page() {
-  const [user, setUser] = useState<User | null>();
-
-  useEffect(() => {
-    return () => {
-      onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser);
-      });
-    };
-  }, []);
-
   return (
     <UserAuthContextProvider>
-      <Logout />
-      <Login />
-      {user ? user.displayName : "No user"}
+      <AuthTesting />
     </UserAuthContextProvider>
   );
 }
